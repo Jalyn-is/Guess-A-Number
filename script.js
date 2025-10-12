7e938b6a2eac33763e355a6fd0ea6da3e03c7bcc
Jalyn Messado <jalynmessado11@gmail.com>
2025-10-11 21:02:34 -0500

    Created a function to get user input

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let gameNumber;
+let playerChoice;
 
 function getNumber () {
     return num = Math.floor(Math.random() * 100)
 }
 
-console.log(getNumber(getNumber))
+// Using .prompt for now because we haven't touched DOM as yet
+
+function getChoice () {
+    prompt(playerChoice, 'Enter a number')
+    return playerChoice;
+}
+console.log(getChoice(playerChoice))
